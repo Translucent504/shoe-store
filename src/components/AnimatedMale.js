ab7c599e91bd186827c9d57ec12676b692c0925e
Translucent504 <fleshlime.1@gmail.com>
2020-07-14 12:52:32 +0500

    Home page ready heavenly style

--- src/components/AnimatedMale.js
+++ src/components/AnimatedMale.js
@@ -2,38 +2,23 @@ import React from 'react'
 import useWebAnimations from "@wellyshen/use-web-animations";
 import img1 from '../images/trans1.png'
 
-let maleStyle = {
-    height: "200px",
-}
-
 const AnimatedMale = ({customStyle}) => {
     const maleAnim = useWebAnimations({
         keyframes: {
-            transform: ["translateY(0px)", "translateY(100px)"],// Go through three colors
+            transform: ["scale(0.5)", "scale(2)"],// Go through three colors
             filter: ["hue-rotate(0deg)","hue-rotate(360deg)"]
         },
         timing: {
             delay: 0, // Start with a 500ms delay
-            duration: 7000, // Run for 1000ms
+            duration: 5000, // Run for 1000ms
             iterations: Infinity, // Repeat once
             direction: "alternate", // Run the animation forwards and then backwards
             easing: "ease-in-out", // Use a fancy timing function
         }
     });
 
-    setInterval(() => {
-        const animation = maleAnim.getAnimation()
-        if (animation.playbackRate > 2) {
-            animation.updatePlaybackRate(animation.playbackRate * 0.9)
-        }
-    }, 1500);
-
-    const handleClick = () => {
-        const animation = maleAnim.getAnimation()
-        animation.updatePlaybackRate(animation.playbackRate * 1.5)
-    }
     return (
-            <img style={{...maleStyle, ...customStyle}} onClick={handleClick} src={img1} ref={maleAnim.ref} alt="" />
+            <img style={{...customStyle}} src={img1} ref={maleAnim.ref} alt="GLORIOUS SHOE" />
     )
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,57 +2,40 @@ import React, { useRef } from 'react'
 import { Link } from 'react-router-dom'
 import useWebAnimations from "@wellyshen/use-web-animations";
 import AnimatedMale from './AnimatedMale'
-import bground from '../images/bground.jpg'
+import bground from '../images/ray3.svg'
 import '../styles/Home.css'
+import heaven from '../audio/heavenly.mp3'
 
 const buttonStyle = {
     position: "absolute",
-    top: "50%",
-    left: "50%"
+    top: "30%",
+    left: "32%"
 }
-
-
 
 const bgStyle = {
     // background: "rgb(250,250,226)",
     // background: "radial-gradient(circle, rgba(250,250,226,1) 13%, rgba(181,240,234,1) 49%, rgba(0,212,255,1) 100%)",
+    position: 'absolute',
     backgroundImage: `url(${bground})`,
     backgroundPosition: 'center',
     backgroundSize: 'cover',
     backgroundRepeat: 'no-repeat',
     width: "100vw",
-    height: "100vh"
+    height: "100vh",
+    margin: "-10px 0 0 -10px"
 }
 
 const Home = () => {
-    const buttonRef = useRef();
-    const { playState } = useWebAnimations({
-        ref: buttonRef,
-        keyframes: {
-            transform: ["scale(2)", "scale(6)"], // Go through three colors
-        },
-        timing: {
-            delay: 0, // Start with a 500ms delay
-            duration: 6000, // Run for 1000ms
-            iterations: Infinity, // Repeat once
-            direction: "alternate", // Run the animation forwards and then backwards
-            easing: "ease-in-out", // Use a fancy timing function
-        }
-    });
-
-    setInterval(() => {
-
-    }, 3000);
-
     return (
         <div style={bgStyle} >
-            <div className="home-container">
-                <AnimatedMale />
-                <Link to="/shoestore">
-                    <button ref={buttonRef} style={buttonStyle}>Enter Shoe Store</button>
-                </Link>
-            </div>
-
+            <Link to="/shoestore" >
+                <AnimatedMale customStyle={buttonStyle} />
+            </Link>
+            <audio autoPlay
+                src={heaven}>
+                Your browser does not support the
+            <code>audio</code> element.
+    </audio>
         </div>
     )
 }
